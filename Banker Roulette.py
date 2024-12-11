35b02889a3263608737c42d1bc56a65280fba8f6
Ravenkao <63787781+Ravenkao@users.noreply.github.com>
2024-12-11 15:35:58 -0500

    Add files via upload

---
+++ Banker Roulette.py
@@ -0,0 +1,23 @@
+import random
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# option 1
+# friends = random.randint(0, 4)
+# if friends == 0:
+#     print("Alice")
+# elif friends == 1:
+#     print("Bob")
+# elif friends == 2:
+#     print("Charlie")
+# elif friends == 3:
+#     print("David")
+# elif friends == 4:
+#     print("Emanuel")
+
+# option 2
+# print(random.choice(friends))
+
+# option 3
+# random_index = random.randint(0,4)
+# print(friends[random_index])
+
